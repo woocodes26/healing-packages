8ce87f937fa1d13cb43af59d2dadf62f42e259b9
woocodes26 <mohamedchalh170@gmail.com>
2025-12-10 02:21:20 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,18 +1,1 @@
-<?php
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit;
-}
 
-// Delete all healing_package posts
-$posts = get_posts( array(
-    'post_type'   => 'healing_package',
-    'numberposts' => -1,
-    'post_status' => 'any',
-) );
-
-foreach ( $posts as $post ) {
-    wp_delete_post( $post->ID, true );
-}
-
-// Delete options
-delete_option( 'healing_packages_settings' );
