9c4f65b329d783a5320782071075fb6eeee402bb
woocodes26 <mohamedchalh170@gmail.com>
2025-12-10 02:20:45 +0100

    Update helpers.php

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,9 +1,1 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
 
-function healing_packages_get_option( $key, $default = '' ) {
-    $options = get_option( 'healing_packages_settings', array() );
-    return isset( $options[ $key ] ) ? $options[ $key ] : $default;
-}
