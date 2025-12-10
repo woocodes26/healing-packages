2aec3824d9c49b4f81df074c30c03b1ee9efdd9d
woocodes26 <mohamedchalh170@gmail.com>
2025-12-10 02:19:11 +0100

    Update frontend.js

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,12 +1,1 @@
-(function($){
-    'use strict';
 
-    $(document).ready(function(){
-        // Simple accordion example (if you later add accordion classes)
-        $('.healing-package-card .healing-package-toggle').on('click', function(e){
-            e.preventDefault();
-            $(this).closest('.healing-package-card').toggleClass('is-open');
-        });
-    });
-
-})(jQuery);
